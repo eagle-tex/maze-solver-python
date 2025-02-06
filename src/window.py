cf296ecf5ce4319bce34846631d3ae8724370416
Gildas Montcho <gmontcho@gmail.com>
2025-02-07 00:45:29 +0100

    feat(window.py): create a 'Window' class

---
+++ src/window.py
@@ -0,0 +1,34 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()  # Create the main window
+        self.__root.title("Maze solver")  # Give it a title
+        self.__canvas = Canvas(self.__root)
+        self.__canvas.pack(fill=BOTH)  # Fill the entire window
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
+
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+
+if __name__ == "__main__":
+    main()
